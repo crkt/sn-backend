167c7655a3314e5326ebcc018de910b8a46df3cc
phcr <philipmayoo@gmail.com>
2015-10-26 14:11:49 +0000

    Added dependencies

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject sn-backend "0.1.0-SNAPSHOT"
+  :description "The backend of the Sort Night application, this is a restful web service."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.4.0"]
+                 ])
