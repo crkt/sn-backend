01adc82cbc015781583716547e1a9200ccee1217
phcr <philipmayoo@gmail.com>
2015-10-27 21:56:22 +0000

    Created a movie record

--- src/sn_backend/domain/movie.clj
+++ src/sn_backend/domain/movie.clj
@@ -5,3 +5,9 @@
 ;; Create a map of fields for the movie.
 ;; Parse json -> movie map
 ;; Create a validation function for a movie.
+
+
+;; defrecord Movie,
+;; The movie with corresponding fields
+;; https://clojuredocs.org/clojure.core/defrecord
+(defrecord Movie [title year runtime genre])
