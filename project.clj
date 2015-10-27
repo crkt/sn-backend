31c101260f2e827c5fba0c616badd12fc5fffea2
phcr <philipmayoo@gmail.com>
2015-10-27 13:51:44 +0000

    Added jdbc dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [ring "1.4.0"]
                  [ring/ring-json "0.4.0"]
                  [compojure "1.4.0"]
