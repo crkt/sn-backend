a74538a4f3b9763e4539fd7727e1bfc39c86104a
phcr <philipmayoo@gmail.com>
2015-10-27 11:44:13 +0000

    Can now take a request and create values/maps from it. Currently returns what you give it.

--- src/sn_backend/core.clj
+++ src/sn_backend/core.clj
@@ -7,10 +7,17 @@
             [ring.middleware.stacktrace :refer :all])
   (:gen-class))
 
+(defn register-movie [req]
+  (let [title (get-in req [:body :title])
+        year (get-in req [:body :year])
+        runtime (get-in req [:body :runtime])
+        genre (get-in req [:body :genre])]
+    (response {:title title :year year :runtime runtime :genre genre})))
 
 (defroutes handler
-  (GET "/" [] (response {:name "Hello"}))
-  (POST "/" [name] (response {:name name}))
+  (GET "/" [] (response {}))
+  (POST "/movie" request
+        (register-movie request))
   (route/not-found "The requested resource does not exist"))
 
 
@@ -21,6 +28,7 @@
    (wrap-json-response)
    (wrap-reload '[sn-backend.core])
    (wrap-stacktrace)))
+
 
 
 
