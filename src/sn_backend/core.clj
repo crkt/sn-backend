9c1253e38e0f8a0000815d4e782c216886f3a5ab
phcr <philipmayoo@gmail.com>
2015-10-27 11:48:43 +0000

    Added comments

--- src/sn_backend/core.clj
+++ src/sn_backend/core.clj
@@ -7,6 +7,10 @@
             [ring.middleware.stacktrace :refer :all])
   (:gen-class))
 
+
+;; register-movie : request map (json) -> response (json)
+;; takes a json request with movie data and creates a map of it
+;; to the register it in the database.
 (defn register-movie [req]
   (let [title (get-in req [:body :title])
         year (get-in req [:body :year])
@@ -14,6 +18,9 @@
         genre (get-in req [:body :genre])]
     (response {:title title :year year :runtime runtime :genre genre})))
 
+;; handler : void -> response
+;; the routing of the application
+;; returns a response depending on the requested resource.
 (defroutes handler
   (GET "/" [] (response {}))
   (POST "/movie" request
@@ -21,6 +28,9 @@
   (route/not-found "The requested resource does not exist"))
 
 
+;; app : void -> void
+;; the app to run on the server
+;; uses ring and compojure handlers to handle routing.
 (def app
   (-> 
    handler
