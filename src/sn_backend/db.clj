82e6295ed1d1557e4f9077eae356e31732e7df0a
phcr <philipmayoo@gmail.com>
2015-10-28 20:46:28 +0000

    adding db queries for adding a new movie to the database

--- src/sn_backend/db.clj
+++ src/sn_backend/db.clj
@@ -1,5 +1,6 @@
 (ns sn-backend.db
   (:require [sn-backend.domain.movie :refer :all]
+            [clojure.string :refer :all]
             [clojure.java.jdbc :refer :all :as jdbc])
   (:gen-class))
 
@@ -21,24 +22,52 @@
            :user "phcr"
            :password "awt800"}))
 
+;; get-genres : map : list
+;; returns just the value of the genre key
+(defn get-genres [row]
+  (:genre row))
 
-
+(defn get-genre-id [row]
+  (:id row))
 
 ;; get-movie-genres : string -> sequence
 ;; get's all the genres of a movie, returns a sequence of genres
 (defn get-movie-genres [id]
   (with-db-connection [db-con *db*]
-    (jdbc/query db-con ["select genre from genre join (select genre_id from movie_genre where movie_id=?) as gm
-       on (gm.genre_id = genre.id)" id])))
+    (let [rs  (jdbc/query db-con ["select genre from genre join (select genre_id from movie_genre where movie_id=?) as gm on (gm.genre_id = genre.id)" id])]
+      (map get-genres rs))))
 
+(defn get-movie-genre-ids [genres]
+  (with-db-connection [db-con *db*]
+    (map (fn [x]
+           (jdbc/query db-con ["select id from genre where genre = ?" 
+                               (lower-case x)])) 
+         genres)))
 
+(defn genre-in? [genre]
+  (with-db-connection [db-con *db*]
+    (let [result (jdbc/query ["select id from genre where genre= ?" (lower-case genre)])]
+      (if (zero? (first result))
+        true
+        false))))
+
+(defn create-movie-genre-relation [movie-id genre-ids]
+  (with-db-connection [db-con *db*]
+    (map (fn [x] (jdbc/insert! db-con :movie_genre
+                              {:genre_id x
+                               :movie_id movie-id}))
+         genre-ids)))
+
+;; create-movie : map -> Movie
+;; takes a map of values for a movie
+;; return a new Movie record.
 (defn create-movie [row]
   (let [genres (get-movie-genres (:id row))
         m (->Movie (:id row)
-                  (:title row)
-                  (:year row)
-                  (:runtime row)
-                  genres)]
+                   (:title row)
+                   (:year row)
+                   (:runtime row)
+                   genres)]
     m))
 
 ;; Test code for the database.
@@ -49,12 +78,20 @@
       (map create-movie rs))))
 
 
-
-
 ;; add-movie : map -> boolean
 ;; adds a movie to the database to persist it.
 ;; returns a boolean if it was successfull or not.
-(defn add-movie [movie])
+(defn add-movie [movie]
+  (with-db-transaction [db-con *db*]
+    (println "MOVIE" movie)
+    (let [movie-id (jdbc/insert! db-con :movie
+                                 {:title (:title movie)
+                                  :year (:year movie)
+                                  :runtime (:runtime movie)})
+          genre-ids (get-movie-genre-ids (:genres movie))]
+      (println "GENRES" genre-ids)
+      (create-movie-genre-relation movie-id genre-ids))))
+
 
 ;; search-movie : map -> movies
 ;; searches the database for movies with the attributes in the search.
