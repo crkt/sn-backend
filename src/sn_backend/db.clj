51f9341fb65fdaf2cf0fdaf29a5ad291822ab5d0
Philip Crockett <philipmayoo@gmail.com>
2015-11-04 19:44:12 +0100

    Updated comments in the backend

--- src/sn_backend/db.clj
+++ src/sn_backend/db.clj
@@ -4,13 +4,6 @@
             [clojure.java.jdbc :refer :all :as jdbc])
   (:gen-class))
 
-;; How to write queries 
-;; https://github.com/clojure/java.jdbc
-;; https://en.wikibooks.org/wiki/Clojure_Programming/Examples/JDBC_Examples
-;; @TODO
-;; Add the JDBC driver
-;; Create a Movie Domain map/vector
-
 ;; Creates a global database variable.
 ;; earmuffs since it's global.
 (let [db-host "localhost"
@@ -22,24 +15,31 @@
            :user "sortnight"
            :password "secret"}))
 
-(defn get-movie-genres [id]
-  "get's all the genres of a movie, returns a sequence of genres"
+(defn get-movie-genres 
+  "get's all the genres of a movie, returns a sequence of genres
+  [{:genre action :genre drama}]"
+  [id]
   (with-db-connection [db-con *db*]
     (jdbc/query db-con ["select genre from genre join (select genre_id from movie_genre where movie_id=?) as gm on (gm.genre_id = genre.id)" id])))
 
-(defn params-sql-query [sql params]
-  "Creates a sql vector that the jdbc expects, changes all symbols into strings in the parameters"
+(defn params-sql-query 
+  "Creates a sql vector that the jdbc expects, 
+  changes all symbols into strings in the parameters. e.g
+  [sql params]
+  [select * from movie where id = ? and name = ? 1 Park]" 
+  [sql params]
   (into [] (flatten (cons sql (map str params)))))
 
-(defn add-args-to-query [lst]
-  "Adds all the ? needed in the preparedStatement, lst is the arguments to count"
+(defn add-args-to-query 
+  "Adds all the ? needed in the preparedStatement, lst is the arguments to count
+  Since prepared statements are not 'dynamic' this is required. You must know all
+  of the ? in advance."
+  [lst]
   (clojure.string/join ", " (take (count lst) (repeat "?"))))
 
-;; create-movie : map -> Movie
-;; takes a map of values for a movie
-;; return a new Movie record.
-(defn create-movie [row]
-  "creates a movie record with a row from the database"
+(defn create-movie 
+  "creates a movie record with a movie row from the database"
+  [row]
   (let [genres (get-movie-genres (:id row))
         m (->Movie (:id row)
                    (:title row)
@@ -48,13 +48,10 @@
                    genres)]
     m))
 
-;; search-movie : map -> movies
-;; searches the database for movies with the attributes in the search.
-;; returns the result of all the movie found.
-(defn search-movie [attributes])
-
-(defn search-for-genres [genres]
-  "Expects a vector of genres, [action drama]"
+(defn search-for-genres 
+  "Searches for movies with the genres given.
+   Expects a vector of genres, [action drama]"
+  [genres]
   (with-db-connection [db-con *db*]
     ;; the query is a sql vector query [sql-query & params]
     (let [query (params-sql-query
@@ -71,26 +68,6 @@
       (map create-movie rs))))
 
 
-
-
-;; delete-movie : string -> boolean
-;; delets the movie in the database with the id supplied.
-;; returns a boolean if it was successfull or not.
-(defn delete-movie [id])
-
-;; create-user-account : user -> boolean
-;; takes information about a user and adds them to the database
-;; returns a boolean if it was successfull or not.
-(defn create-user-account [user])
-
-
-;; delete-user-account : string -> boolean
-;; removes a user from the database
-;; returns a boolean if it was successfull or not.
-(defn delete-user-account [id])
-
-
-;; add-user-to-group : string, string -> boolean
-;; takes a user id and group id and creates a relation 
-;; between the user and the group, so the user belongs to the group.
-(defn add-user-to-group [user, group])
+(defn search-movie 
+  "Unsupported function"
+  [attributes])
