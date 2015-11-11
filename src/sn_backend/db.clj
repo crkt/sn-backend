11f790b3cad8bd415703b734dc26dd33ae32466a
phcr <philipmayoo@gmail.com>
2015-11-11 16:59:14 +0000

    Search by query is now working

--- src/sn_backend/db.clj
+++ src/sn_backend/db.clj
@@ -5,36 +5,66 @@
             [clojure.java.jdbc :refer :all :as jdbc])
   (:gen-class))
 
-;; Movie record
+;;*****************************************************
+;; Movie Record
+;;*****************************************************
 (defrecord Movie [id title year runtime genres])
 
-;; Creates a global database variable.
-;; earmuffs since it's global.
-(defdb *db (mysql {:db "sortnight"
-                   :user "sortnight"
-                   :password "secret"
-                   :host "localhost"
-                   :port "3306"
-                   :delimiters ""}))
+;;*****************************************************
+;; Database connection config
+;;*****************************************************
+(defdb ^{:dynamic false} db (mysql {:db "sortnight"
+                       :user "sortnight"
+                       :password "secret"
+                       :host "localhost"
+                       :port "3306"
+                       :delimiters ""}))
 
+;;*****************************************************
+;; Base queries
+;;*****************************************************
 
+;; genres-q : vector -> vector
+(defn genres-q 
+  "Get all id's for genres specified. Returns a vector of the ids
+  [crime drama action adventure] -> [1 2 3 4]"
+  [genres]
+  (into [] (map (fn [x]
+                  (:id x)) (select "genre" 
+                  (fields :id)
+                  (where {:genre.genre [in genres]})))))
 
-(select "genre"
-        (where {:genre.genre [in ["crime"]]}))
+;; movie-genres : vector -> vector
+(defn movie-genres-q
+  "Returns movie id's in a vector
+  [crime drama] -> [1 2 3] (Consists of movie_ids)
+  that have the genres specified."
+  [genres]
+  (into [] (map (fn [x]
+                  (:movie_id x)) (select "movie_genre"
+                        (fields :movie_id)
+                        (where {:movie_genre.genre_id [in (genres-q genres)]})))))
 
-(defn movie-genres 
+;; all-movie-genres : number -> seq({:genre ""},{:genre ""})
+(defn all-movie-genres
   "Get the movies genres"
   [id]
-  (select "genre"
-          (fields :genre)
-          (join "movie_genre" (= :movie_genre.movie_id id))
-          (where (= :genre.id :movie_genre.genre_id))))
+  (into [] (map (fn [x]
+                  (:genre x)) 
+                (select "genre"
+                        (fields :genre)
+                        (join "movie_genre" (= :movie_genre.movie_id id))
+                        (where (= :genre.id :movie_genre.genre_id))))))
 
-(defmacro genres [genres]
-  `(select "genre" (where {:genre.genre [in ~genres]})))
 
-(defn create-movie [row]
-  (let [genres (movie-genres (:id row))
+;;*****************************************************
+;; Movie Record creation
+;;*****************************************************
+;; create-movie : {:key val...} -> Movie Record
+(defn create-movie 
+  "Creates a movie record from a hash of movie values"
+  [row]
+  (let [genres (all-movie-genres (:id row))
         m (->Movie (:id row)
                    (:title row)
                    (:year row)
@@ -42,23 +72,14 @@
                    genres)]
     m))
 
-;;
-;;         "select * from movie where id in "
-;;                  "(select movie_id from movie_genre where genre_id in"
-;;
-;;                  "(select id from genre where genre in ("
-;;
-;; (where {:email.id [in (subselect email
-;;                          (fields :id)
-;;                          (where {:sent true})
-;;
-;;
+;;*****************************************************
+;; Search queries
+;;*****************************************************
 
-(defn search-movie
-  [{:keys [genres year runtime]}]
-  (select "movie"
-            (where {:movie.id [in (subselect "movie_genre"
-                                             (fields :movie_id)
-                                             (where {:genre_id [in (subselect "genre"
-                                                                              (fields :genre)
-                                                                              (where {:genre.genre [in genres]}))]}))]})))
+;; all-movies-with-genres : vector -> seq
+(defn all-movie-with-genres 
+  "Returns movie objects with the genres specified.
+  [crime drama] -> (#movieObj #movieObj)"
+  [genres]
+  (map create-movie (select "movie"
+               (where {:id [in (movie-genres genres)]}))))
