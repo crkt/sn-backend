e2b5dae795ffa0fe75c13a471922e8e5064609af
phcr <philipmayoo@gmail.com>
2015-11-12 19:32:59 +0000

    Fixed search for any attribute

--- src/sn_backend/search.clj
+++ src/sn_backend/search.clj
@@ -4,19 +4,25 @@
 
 
 
-;;
-;; base sql ->
-;; conditions
-;;
-;;
-
 (defn not-nil? [x]
   (not (nil? x)))
 
 
-(defn search-movie [body]
+(defn search-movie 
+  [body]
   (let [genres (:genres body)
         runtime (:runtime body)
         year (:year body)]
     (cond
-     (and (not-nil? genres) (not-nil? runtime) (not-nil? year)) (db/movie-with-attributes :genres genres, :runtime runtime, :year year))))
+     (and (not-nil? genres) (not-nil? runtime) (not-nil? year)) 
+     (db/movie-with-attributes :genres genres, :runtime runtime, :year year)
+     (and (not-nil? genres) (not-nil? runtime) (nil? year))
+     (db/movies-with-attributes :genres genres :runtime runtime)
+     (and (not-nil? genres) (nil? runtime) (nil? year))
+     (db/movies-with-attributes :genres genres)
+     (and (nil? genres) (not-nil? runtime) (not-nil? year))
+     (db/movies-with-attributes :runtime runtime :year year)
+     (and (nil? genres (not-nil? runtime) (nil? year)))
+     (db/movies-with-attributes :runtime runtime)
+     (and (nil? genres) (nil? runtime) (not-nil? year))
+     (db/movies-with-attributes :year year))))
