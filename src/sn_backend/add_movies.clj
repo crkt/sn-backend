ab685785185e64ff459dcb96c98d9f11cf7eaa7d
Philip Crockett <philipmayoo@gmail.com>
2015-12-10 14:01:30 +0100

    moved code to db.clj

--- src/sn_backend/add_movies.clj
+++ src/sn_backend/add_movies.clj
@@ -2,7 +2,3 @@
 	(:require [sn-backend.db :as db])
 	(:gen-class))
 	
-(defn read-movies []
-	(let [movies (db/read-from-file-with-trusted-content "movies.clj")]
-		(map db/insert-movie movies)))	
-	
